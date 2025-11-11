46a85b685471157a90ff4fb15c84271669587a4b
harsha314 <harshavardhan32952@gmail.com>
2025-11-11 12:12:07 +0530

    JSONParser completed

--- src/main/java/com/questions/jsonparser/JSONParser.java
+++ src/main/java/com/questions/jsonparser/JSONParser.java
@@ -1,5 +1,6 @@
 package com.questions.jsonparser;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,11 +19,23 @@ public class JSONParser {
     switch (type) {
       case LEFT_CURLY_BRACKET:
         return parseObject();
-
+      case LEFT_BRACKET:
+        return parseArray();
+      case STRING:
+        String s = current.getContent();
+        return s;
+      case NUMBER:
+        double num = Double.parseDouble(current.getContent());
+        return num;
+      case BOOLEAN:
+        boolean value = Boolean.parseBoolean(current.getContent());
+        return value;
+      case NULL:
+        current = jsonTokenizer.nextToken();
+        return null;
       default:
-        break;
+        throw new RuntimeException("Unexpected token" + current.getTokenType());
     }
-    return null;
   }
 
   private Map<String, Object> parseObject() {
@@ -51,7 +64,21 @@ public class JSONParser {
   }
 
   private List<Object> parseArray() {
-    return null;
+    List<Object> list = new ArrayList<>();
+    expect(TokenType.LEFT_BRACKET);
+
+    if (current.getTokenType() == TokenType.RIGHT_BRACKET) {
+      jsonTokenizer.nextToken();
+      return list;
+    }
+    do {
+      list.add(parseValue());
+      if (current.getTokenType() == TokenType.COMMA) {
+        current = jsonTokenizer.nextToken();
+      } else
+        break;
+    } while (true);
+    return list;
   }
 
   private void expect(TokenType type) {
